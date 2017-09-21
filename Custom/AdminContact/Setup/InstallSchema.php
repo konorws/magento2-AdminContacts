34ea84b8b15fe811621023e668d7e8af84d3bfcd
konorws <yatzyshyn.pitbulls@gmail.com>
2017-09-22 02:37:54 +0300

    Add Setup/InstallSchema

---
+++ Custom/AdminContact/Setup/InstallSchema.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Custom\AdminContact\Setup;
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\DB\Adapter\AdapterInterface;
+
+class InstallSchema implements InstallSchemaInterface
+{
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $installer = $setup;
+        $installer->startSetup();
+
+        // Create table 'contact_posts'         
+        $table = $installer->getConnection()->newTable(
+            $installer->getTable('contact_posts')
+        )->addColumn(
+            'post_id',
+            \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+            null,
+            ['identity' => true, 'nullable' => false, 'primary' => true],
+            'Post ID'
+        )->addColumn(
+            'name',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            255,
+            ['nullable' => true],
+            'Name user'
+        )->addColumn(
+            'email',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            255,
+            ['nullable' => true],
+            'email user'
+        )->addColumn(
+            'telephone',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            255,
+            ['nullable' => true],
+            'Telephone user'
+        )->addColumn(
+            'comment',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            '2M',
+            [],
+            'User message'
+        )->addColumn(
+            'answer',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            '2M',
+            [],
+            'Answer administration'
+        )->addColumn(
+            'creation_time',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+            null,
+            [],
+            'Creation Time'
+        )->addColumn(
+            'status',
+            \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+            null,
+            ['nullable' => false, 'default' => '0'],
+            'Is answer added'
+        )->addIndex(
+            $installer->getIdxName('contact_posts', ['post_id']),
+            ['post_id']
+        )->addIndex(
+            $setup->getIdxName(
+                $installer->getTable('contact_posts'),
+                ['post_id', 'status'],
+                AdapterInterface::INDEX_TYPE_FULLTEXT
+            ),
+            ['post_id', 'status'],
+            ['type' => AdapterInterface::INDEX_TYPE_FULLTEXT]
+        )->setComment(
+            'Contact Posts table'
+        );
+        $installer->getConnection()->createTable($table);
+        
+        $installer->endSetup();
+    }
+}
