c0e4eeedabc5c9eeb7140c34a77ae8510f0d7d80
konorws <yatzyshyn.pitbulls@gmail.com>
2017-09-26 04:16:29 +0300

    Create Routers and Controller

---
+++ Custom/AdminContact/Controller/adminhtml/Posts/Index.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Custom\AdminContact\Controller\Adminhtml;
+
+Class Index extends \Magento\Backend\App\Action
+{
+
+    public function execute()
+    {
+         echo ("hello World");
+    }
+}
+
+?>
