3f3f649e454fed7e206c06bd84d6cbd2ae586d0a
konorws <yatzyshyn.pitbulls@gmail.com>
2017-09-27 19:16:53 +0300

    add action UIComponent

---
+++ Custom/AdminContact/UI/Component/GridViewAction.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Custom\AdminContact\Ui\Component;
+
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+
+/**
+ * Class ViewAction
+ */
+class GridViewAction extends Column
+{
+
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = []
+    ) {
+        $this->urlBuilder = $urlBuilder;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as & $item) {
+                if (isset($item['post_id'])) {
+                    $viewUrlPath = $this->getData('config/viewUrlPath') ?: '#';
+                    $urlEntityParamName = $this->getData('config/urlEntityParamName') ?: 'post_id';
+                    $item[$this->getData('name')] = [
+                        'view' => [
+                            'href' => $this->urlBuilder->getUrl(
+                                $viewUrlPath,
+                                [
+                                    $urlEntityParamName => $item['post_id']
+                                ]
+                            ),
+                            'label' => __( ($this->getData('config/Label') ?: 'Edit') )
+                        ]
+                    ];
+                }
+            }
+        }
+
+        return $dataSource;
+    }
+}
